115727635270204b1c443cbc0bff122c6a83e3a5
Chandler Puckett <chandler.puckett1@gmail.com>
2020-11-17 14:23:31 -0800

    sign in confirmation activity complete

--- app/src/main/java/com/cinnamontoast/scavengerhunt/SignupConfirmationActivity.java
+++ app/src/main/java/com/cinnamontoast/scavengerhunt/SignupConfirmationActivity.java
@@ -2,7 +2,14 @@ package com.cinnamontoast.scavengerhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.amazonaws.mobile.auth.ui.SignInActivity;
+import com.amplifyframework.core.Amplify;
 
 public class SignupConfirmationActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class SignupConfirmationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_confirmation);
+
+        ((Button) findViewById(R.id.confirmBtn)).setOnClickListener(view -> {
+            String username = ((TextView)findViewById(R.id.confirmUser)).getText().toString();
+            String code = ((TextView)findViewById(R.id.confirmCode)).getText().toString();
+
+            Amplify.Auth.confirmSignUp(
+                    username,
+                    code,
+                    result -> {
+                        Log.i("Amplify.confirm", result.isSignUpComplete() ? "Confirm Signup Succeeded" :
+                                "Confirm SignUp Not Complete");
+                        startActivity(new Intent(SignupConfirmationActivity.this, SignInActivity.class));
+                    },
+                    error -> Log.e("Amplify.confirm", error.toString())
+            );
+        });
     }
 }
