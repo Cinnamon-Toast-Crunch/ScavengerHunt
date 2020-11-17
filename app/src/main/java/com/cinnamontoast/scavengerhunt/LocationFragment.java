b3ff87d23709a0f54ee0d3f1cb58501de13cadc2
mdwohl <mdwohl@gmail.com>
2020-11-17 14:39:25 -0800

    added recycler views for locations and hunts

---
+++ app/src/main/java/com/cinnamontoast/scavengerhunt/LocationFragment.java
@@ -0,0 +1,59 @@
+package com.cinnamontoast.scavengerhunt;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LocationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LocationFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "name";
+
+    // TODO: Rename and change types of parameters
+    private String mName;
+
+    public LocationFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param name Parameter 1.
+     * @return A new instance of fragment LocationFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LocationFragment newInstance(String name) {
+        LocationFragment fragment = new LocationFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, name);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mName = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_location, container, false);
+    }
+}
--- app/src/main/java/com/cinnamontoast/scavengerhunt/MainActivity.java
+++ app/src/main/java/com/cinnamontoast/scavengerhunt/MainActivity.java
@@ -13,15 +13,19 @@ import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Hunt;
+import com.amplifyframework.datastore.generated.model.Location;
 import com.amplifyframework.datastore.generated.model.Quest;
 import com.amplifyframework.datastore.generated.model.User;
 import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements QuestAdapter.QuestListFormatter {
+public class MainActivity extends AppCompatActivity implements QuestAdapter.QuestListFormatter, LocationAdapter.LocationListFormatter, HuntAdapter.HuntListFormatter {
 
     ArrayList<Quest> quests = new ArrayList<>();
+    ArrayList<Location> locations = new ArrayList<>();
+    ArrayList<Hunt> hunts = new ArrayList<>();
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -40,7 +44,25 @@ public class MainActivity extends AppCompatActivity implements QuestAdapter.Ques
         questRecycler.setLayoutManager(new LinearLayoutManager(this));
         questRecycler.setAdapter(new QuestAdapter(quests,this));
 
-        }
+        Location tempLocation = addFakeLocation("space station", tempUser);
+        locations.add(addFakeLocation("space station", tempUser));
+        locations.add(addFakeLocation("the moon", tempUser));
+        locations.add(addFakeLocation("low earth orbit", tempUser));
+
+        RecyclerView locationRecycler = findViewById(R.id.locationRecycler);
+        locationRecycler.setLayoutManager(new LinearLayoutManager(this));
+        locationRecycler.setAdapter(new LocationAdapter(locations, this));
+
+        Hunt tempHunt = addFakeHunt("find the freeze dried ice cream", 10, tempLocation);
+        hunts.add(addFakeHunt("pilot space station", 15, tempLocation));
+        hunts.add(addFakeHunt("visit the moon", 25, tempLocation));
+        hunts.add(addFakeHunt("enter low earth orbit", 50,tempLocation));
+
+        RecyclerView huntRecycler = findViewById(R.id.huntRecycler);
+        huntRecycler.setLayoutManager(new LinearLayoutManager(this));
+        huntRecycler.setAdapter(new HuntAdapter(hunts, this));
+
+    }
     public Quest addFakeQuest(String title, User user){
         Quest quest = Quest.builder().userId(user.getId()).title(title).build();
         return quest;
@@ -49,6 +71,14 @@ public class MainActivity extends AppCompatActivity implements QuestAdapter.Ques
     public User addFakeUser(String username, String email){
         User user = User.builder().userName(username).email(email).build();
         return user;
+    }
+    public Location addFakeLocation(String name, User user){
+        Location location = Location.builder().userId(user.getId()).name(name).build();
+        return location;
+    }
+    public Hunt addFakeHunt(String name, Integer totalPoints, Location location){
+        Hunt hunt = Hunt.builder().locationId(location.getId()).name(name).totalPoints(totalPoints).build();
+        return hunt;
     }
 
     public void configAWS() {
@@ -68,6 +98,16 @@ public class MainActivity extends AppCompatActivity implements QuestAdapter.Ques
 
     @Override
     public void questFormatter(Quest quest) {
+
+    }
+
+    @Override
+    public void locationFormatter(Location location) {
+
+    }
+
+    @Override
+    public void huntFormatter(Hunt hunt) {
 
     }
 }
