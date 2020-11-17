68446e28b1dd4ab2f09ab0360fc07f34c35cc1ac
daviddicken <davidfromseattle@gmail.com>
2020-11-16 18:06:12 -0800

    added aws config method

--- app/src/main/java/com/cinnamontoast/scavengerhunt/MainActivity.java
+++ app/src/main/java/com/cinnamontoast/scavengerhunt/MainActivity.java
@@ -1,14 +1,44 @@
 package com.cinnamontoast.scavengerhunt;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configAWS();
+
+        }
+
+
+    public void configAWS() {
+        //configAWS();
+        try {
+            Amplify.addPlugin(new AWSS3StoragePlugin());
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.configure(getApplicationContext());
+            Log.i("MyAmplifyApp", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
+        }
+
+
     }
 }
