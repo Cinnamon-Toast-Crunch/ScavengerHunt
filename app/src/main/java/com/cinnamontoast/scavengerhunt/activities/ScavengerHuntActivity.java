881d918deea4162ae360ca79d4a9f5e3ae2820a2
Chandler Puckett <chandler.puckett1@gmail.com>
2020-11-18 14:18:20 -0800

    deep link is opening the activity

--- app/src/main/java/com/cinnamontoast/scavengerhunt/activities/ScavengerHuntActivity.java
+++ app/src/main/java/com/cinnamontoast/scavengerhunt/activities/ScavengerHuntActivity.java
@@ -1,16 +1,65 @@
 package com.cinnamontoast.scavengerhunt.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.cinnamontoast.scavengerhunt.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 
 public class ScavengerHuntActivity extends AppCompatActivity {
+
+    private static final String TAG = "no worky";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scavenger_hunt);
+
+        Log.i("DEEP LINKS", "---- Deep Link Works ----");
+
+        // Handle Hunt DeepLink
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+
+
+//       ----- FIREBASE ZOMBIE CODE MIGHT NOT NEED IT -----
+//        FirebaseDynamicLinks.getInstance()
+//                .getDynamicLink(getIntent())
+//                .addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
+//                    @Override
+//                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+//                        // Get deep link from result (may be null if no link is found)
+//                        Uri deepLink = null;
+//                        if (pendingDynamicLinkData != null) {
+//                            deepLink = pendingDynamicLinkData.getLink();
+//                        }
+//
+//
+//                        // Handle the deep link. For example, open the linked
+//                        // content, or apply promotional credit to the user's
+//                        // account.
+//                        // ...
+//
+//                        // ...
+//
+//
+//                    }
+//                })
+//                .addOnFailureListener(this, new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.e(TAG, "getDynamicLink:onFailure", e);
+//                    }
+//                });
+
     }
 }
