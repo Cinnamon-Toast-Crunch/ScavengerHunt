9a02f983b1c1d6a6df776f7b5c61a047d24e1ca9
Jack Nelson <jnelson.java@gmail.com>
2020-11-19 14:32:52 -0800

    track user related to auth and sign confirmation auto login

--- app/src/main/java/com/cinnamontoast/scavengerhunt/activities/SignupConfirmationActivity.java
+++ app/src/main/java/com/cinnamontoast/scavengerhunt/activities/SignupConfirmationActivity.java
@@ -2,13 +2,17 @@ package com.cinnamontoast.scavengerhunt.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.auth.AuthUser;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.User;
 import com.cinnamontoast.scavengerhunt.R;
@@ -25,13 +29,23 @@ public class SignupConfirmationActivity extends AppCompatActivity {
             String code = ((TextView)findViewById(R.id.confirmCode)).getText().toString();
 
             // TODO: Confirm that this will Auto-Login the user
+            // TODO: Change confirmation to redirect to profile
             Amplify.Auth.confirmSignUp(
                     username,
                     code,
                     result -> {
                         Log.i("Amplify.confirm", result.isSignUpComplete() ? "Confirm Signup Succeeded" :
                                 "Confirm SignUp Not Complete");
-                        copySignedInUserToModel();
+
+                        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                        String pass = preferences.getString("password", "none");
+                        autoSignInOnConfirmation(username, pass);
+
+                        // clear pass from preferences
+                        SharedPreferences.Editor prefEdit = preferences.edit();
+                        prefEdit.putString("password","");
+                        prefEdit.apply();
+
                         startActivity(new Intent(SignupConfirmationActivity.this, ParentProfileActivity.class));
                     },
                     error -> Log.e("Amplify.confirm", error.toString())
@@ -39,8 +53,17 @@ public class SignupConfirmationActivity extends AppCompatActivity {
         });
     }
 
-    public void autoSignInOnConfirmation() {
-
+    public void autoSignInOnConfirmation(String name, String pass) {
+        Amplify.Auth.signIn(
+                name,
+                pass,
+                result -> {
+                    Log.i("Amplify.login", result.isSignInComplete() ? "Sign In Succesful" :
+                            "Sign in failed");
+                    copySignedInUserToModel();
+                },
+                error -> Log.e("Amplify.login", error.toString())
+        );
     }
 
     public void copySignedInUserToModel() {
